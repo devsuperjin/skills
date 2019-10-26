ee9673d1c001b379b69c55be7aaf9d316ed93213
xclouds <50068476+xcloudss@users.noreply.github.com>
2019-10-26 18:48:52 +0800

    Create user-data.cs

---
+++ user-data.cs
@@ -0,0 +1,24 @@
+#!/bin/bash
+sleep 10
+yum install memcached -y
+systemctl start memcached
+wget http://onlyellow.cstor.cn/q4/server_route_v1 -O /root/server_route_v1
+wget http://onlyellow.cstor.cn/q4/conf.toml -O /root/conf.toml
+cd /root
+sed -i 's/memcache_host ="127.0.0.1"/memcache_host ="rou.j2u8sc.0001.cnw1.cache.amazonaws.com.cn"/g' /root/conf.toml
+chmod +x server_route_v1
+nohup ./server_route_v1 conf.toml &
+
+---------------------
+
+#!/bin/bash
+sleep 10
+yum install memcached -y
+systemctl start memcached
+wget http://onlyellow.cstor.cn/q4/server_route_v2 -O /root/server_route_v2
+wget http://onlyellow.cstor.cn/q4/conf.toml -O /root/conf.toml
+cd /root
+chmod +x server_route_v2
+nohup ./server_route_v2 conf.toml &
+
+-------------------------------
