6c23adba3632470d36973201572e8ba733291bbc
xclouds <50068476+xcloudss@users.noreply.github.com>
2019-10-26 18:54:51 +0800

    Update user-data.cs

--- user-data.cs
+++ user-data.cs
@@ -12,12 +12,11 @@ nohup ./server_route_v1 conf.toml &
 ---------------------
 
 #!/bin/bash
-sleep 10
-yum install memcached -y
-systemctl start memcached
+sleep 5
 wget http://onlyellow.cstor.cn/q4/server_route_v2 -O /root/server_route_v2
 wget http://onlyellow.cstor.cn/q4/conf.toml -O /root/conf.toml
 cd /root
+sed -i 's/memcache_host ="127.0.0.1"/memcache_host ="rou.j2u8sc.0001.cnw1.cache.amazonaws.com.cn"/g' /root/conf.toml
 chmod +x server_route_v2
 nohup ./server_route_v2 conf.toml &
 
