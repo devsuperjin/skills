acfb4f7547fd42f6ba7cefbf246dcbf92f72a7d1
xclouds <50068476+xcloudss@users.noreply.github.com>
2019-11-12 15:59:21 +0800

    Update user-data.cs

--- user-data.cs
+++ user-data.cs
@@ -2,22 +2,11 @@
 sleep 10
 yum install memcached -y
 systemctl start memcached
-wget http://onlyellow.cstor.cn/q4/server_route_v1 -O /root/server_route_v1
-wget http://onlyellow.cstor.cn/q4/conf.toml -O /root/conf.toml
+wget -O /root/
+wget -O /root/
 cd /root
-sed -i 's/memcache_host ="127.0.0.1"/memcache_host ="rou.j2u8sc.0001.cnw1.cache.amazonaws.com.cn"/g' /root/conf.toml
-chmod +x server_route_v1
-nohup ./server_route_v1 conf.toml &
+sed -i 's/memcache_host ="127.0.0.1"/memcache_host ="rou.j2u8sc.0001.cnw1.cache.amazonaws.com.cn"/g' /root/
+chmod +x 
+nohup ./
 
 ---------------------
-
-#!/bin/bash
-sleep 5
-wget http://onlyellow.cstor.cn/q4/server_route_v2 -O /root/server_route_v2
-wget http://onlyellow.cstor.cn/q4/conf.toml -O /root/conf.toml
-cd /root
-sed -i 's/memcache_host ="127.0.0.1"/memcache_host ="rou.j2u8sc.0001.cnw1.cache.amazonaws.com.cn"/g' /root/conf.toml
-chmod +x server_route_v2
-nohup ./server_route_v2 conf.toml &
-
--------------------------------
