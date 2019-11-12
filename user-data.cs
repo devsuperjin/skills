7553204e3276e30bbe13f3ae2bf9a00bfde80f16
xclouds <50068476+xcloudss@users.noreply.github.com>
2019-11-12 20:00:40 +0800

    Update user-data.cs

--- user-data.cs
+++ user-data.cs
@@ -1,12 +1,10 @@
 #!/bin/bash
-sleep 10
-yum install memcached -y
-systemctl start memcached
-wget -O /root/
-wget -O /root/
-cd /root
-sed -i 's/memcache_host ="127.0.0.1"/memcache_host ="rou.j2u8sc.0001.cnw1.cache.amazonaws.com.cn"/g' /root/
-chmod +x 
-nohup ./
+sleep 5
+wget [] -O /root/
+wget [] -O /root/
+cd /root[]
+sed -i 's/[]/[]/g' /root/[]
+chmod +x []
+nohup /[]
 
 ---------------------
